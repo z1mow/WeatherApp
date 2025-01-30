291fcdcad167e21b64340b1814fb588a9edfa16f
Şakir Yılmaz Öğüt <zimowcs@gmail.com>
2025-01-30 15:09:07 +0300

    Add initial UI design with weather details, hourly and daily forecast views
    - Add main weather view with temperature and weather condition
    - Add weather details section (humidity, wind, pressure)
    - Create hourly forecast collection view
    - Create daily forecast table view
    - Set up basic layout constraints and styling

--- WeatherApp/Views/WeatherViewController.swift
+++ WeatherApp/Views/WeatherViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class WeatherViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
